96e8e85caaecb83e8f4d4c218de4b1c8bc73b940
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-07-15 11:21:21 +0700

    feat(auth): implement clerk authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import Logo from "@/components/Logo"
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="relative flex h-screen w-full flex-col items-center justify-center">
+            <Logo />
+            <div className="mt-12">
+                {children}
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { HandCoins } from 'lucide-react'
+import Link from 'next/link'
+
+export default function Logo() {
+    return (
+        <Link href="/" className="flex items-center gap-2">
+            <HandCoins className="stroke h-11 w-11 stroke-teal-500 stroke-[1.5]" />
+            <p className="bg-gradient-to-r from-teal-500 to-teal-600 bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent">
+                FinScope
+            </p>
+        </Link>
+    )
+}
