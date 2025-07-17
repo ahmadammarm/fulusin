715ac054dfd0fd555b24e032f8b4b46531a9a777
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-07-17 12:31:16 +0700

    feat(dashboard): add dashboard page function

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,31 @@
-export default function DashboardPage() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+
+    try {
+        const user = await currentUser();
+
+        if (!user) {
+            redirect("/sign-in");
+        }
+
+        const currencySettings = await prisma.currencySettings.findUnique({
+            where: {
+                userId: user.id,
+            },
+        });
+
+        if (!currencySettings) {
+            redirect("/wizard");
+        }
+
+    } catch (error: any) {
+        console.error("Error fetching user data:", error);
+    }
+
     return (
         <div>
             Hello Page
