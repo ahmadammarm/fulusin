6e1649ac3fe0c4991bac2ad258d7e3704ad0e052
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-07-18 07:53:47 +0700

    feat(transaction): add transaction form dialog component

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,34 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import CreateTransactionDialog from "@/components/CreateTransactionDialog";
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
 
-    try {
-        const user = await currentUser();
+    const user = await currentUser();
 
-        if (!user) {
-            redirect("/sign-in");
-        }
-
-        const currencySettings = await prisma.currencySettings.findUnique({
-            where: {
-                userId: user.id,
-            },
-        });
-
-        if (!currencySettings) {
-            redirect("/wizard");
-        }
-
-    } catch (error: any) {
-        console.error("Error fetching user data:", error);
+    if (!user) {
+        redirect("/sign-in");
     }
 
+    const currencySettings = await prisma.currencySettings.findUnique({
+        where: {
+            userId: user.id,
+        },
+    });
+
+    if (!currencySettings) {
+        redirect("/wizard");
+    }
+
+
     return (
-        <div>
-            Hello Page
+        <div className="h-full bg-background">
+            <div className="border-b bg-card">
+                <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+                    <p className="text-3xl font-bold ml-5">
+                        Hello, {user.firstName}!
+                    </p>
+                    <div className="flex items-center gap-4 md:ml-0 ml-5">
+                        <CreateTransactionDialog trigger={
+                            <Button className="border-emerald-500 bg-emerald-800 text-white hover:bg-emerald-950 hover:text-white">
+                                New Income
+                            </Button>
+                        } type="income" />
+                        <CreateTransactionDialog trigger={
+                            <Button className="border-rose-500 bg-rose-800 text-white hover:bg-rose-950 hover:text-white">
+                                New Expense
+                            </Button>
+                        } type="expense" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/CreateTransactionDialog.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { TransactionType } from "@/lib/types";
+import { ReactNode } from "react"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { CreateTransactionSchema, CreateTransactionSchemaType } from "@/schemas/transaction";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
+
+interface Props {
+    trigger: ReactNode;
+    type: TransactionType
+}
+
+export default function CreateTransactionDialog({ trigger, type }: Props) {
+
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+            type,
+            date: new Date(),
+        },
+    })
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                {trigger}
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Create a new <span className={cn("m-1", type === "income" ? "text-green-500" : "text-red-500")}>{type === "income" ? "Income" : "Expense"}</span> transaction
+                    </DialogTitle>
+                </DialogHeader>
+                <Form {...form}>
+                    <form className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Input defaultValue={''} {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Transaction description (optional)
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="amount"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Amount</FormLabel>
+                                    <FormControl>
+                                        <Input defaultValue={0} {...field} type="number" />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Transaction amount (required)
+                                    </FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-2 justify-between">
+                            
+                        </div>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    )
+}
