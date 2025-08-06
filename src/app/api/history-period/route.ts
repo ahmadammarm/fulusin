a0baf26384a6a435df17c9c552e1c98542a4d44b
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-08-06 17:00:57 +0700

    feat(history): add history period api route

---
+++ src/app/api/history-period/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const session = await auth();
+    const user = session?.user;
+
+    if (!user) {
+        redirect("/sign-in");
+    }
+
+    const period = await getHistoryPeriod(user.id);
+    return NextResponse.json({
+        period: period,
+    });
+
+}
+
+export type GetHistoryPeriodResponseType = Awaited<ReturnType<typeof getHistoryPeriod>>;
+
+async function getHistoryPeriod(userId: string) {
+    const result = await prisma.monthHistory.findMany({
+        where: {
+            userId: userId
+        },
+        select: {
+            year: true,
+        },
+        distinct: ["year"],
+        orderBy: [
+            {
+                year: "asc"
+            }
+        ]
+    })
+
+    const years = result.map(item => item.year);
+
+    if (years.length === 0) {
+        return [new Date().getFullYear()];
+    }
+
+    return years;
+
+}
--- src/components/HistoryPeriodSelector.tsx
+++ src/components/HistoryPeriodSelector.tsx
@@ -1,19 +1,32 @@
-import { Timeframe } from "@/lib/types";
+import { Period, Timeframe } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface HistoryPeriodSelectorProps {
+    period: Period;
+    setPeriod: (period: Period) => void;
+    timeframe: Timeframe;
+    setTimeframe: (timeframe: Timeframe) => void;
+}
 
 export default function HistoryPeriodSelector({
     timeframe,
     setTimeframe,
     period,
     setPeriod,
-}: {
-    timeframe: Timeframe;
-    setTimeframe: (timeframe: Timeframe) => void;
-    period: { month: number; year: number };
-    setPeriod: (period: { month: number; year: number }) => void;
-}) {
+}: HistoryPeriodSelectorProps) {
+
+    const historyPeriods = useQuery({
+        queryKey: ["overview", "history-periods"],
+        queryFn: async () => {
+            const response = await axios.get("/api/history-period");
+            return response.data.data;
+        }
+    });
+
     return (
-        <div className="flex items-center gap-4">
-        
+        <div className="flex flex-wrap items-center gap-4">
+            
         </div>
     );
 }
