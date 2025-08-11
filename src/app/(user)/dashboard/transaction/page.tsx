158e5c3b03969a86e5c3c79dc56f6b1c9e52e56d
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-08-11 10:37:34 +0700

    feat(transaction): add transaction page

---
+++ src/app/(user)/dashboard/transaction/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export default function TransactionPage() {
+    return (
+        <div className="border-b bg-card">
+            <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+                <div>
+                    <p className="text-3xl font-bold">
+                        Transactions
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
