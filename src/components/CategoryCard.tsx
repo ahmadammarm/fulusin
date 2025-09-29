3270da65f5a7a23c3d607b3ac7fc4f12061c4f2d
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-09-29 07:02:05 +0700

    refactor: add margin in each category card

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -6,7 +6,7 @@ import CategoryDeleteDialog from "./CategoryDeleteDialog";
 export default function CategoryCard({ category }: { category: Category }) {
     return (
         <div className="flex border-separate flex-col justify-between rounded-md border shadow-md">
-            <div className="flex flex-col items-center gap-2 p-4">
+            <div className="flex flex-col items-center gap-2 p-4 mr-3">
                 <span className="text-3xl" role="img">{category.icon}</span>
                 <span className="text-md text-white font-bold">{category.name}</span>
             </div>
