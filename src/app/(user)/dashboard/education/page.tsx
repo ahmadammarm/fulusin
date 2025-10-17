50a9ddd437a3ff28b3d91ed7b0b9783f4ed86f89
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-10-17 21:32:40 +0700

    refactor: education page

--- src/app/(user)/dashboard/education/page.tsx
+++ src/app/(user)/dashboard/education/page.tsx
@@ -1,10 +1,23 @@
-import Education from "@/components/Education";
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function EducationPage() {
+
+    const router = useRouter()
+
+    const handleDashboard = () => {
+        router.push("/dashboard");
+    }
+
     return (
-        <>
-            <Education />
-        </>
+        <div className="flex flex-col items-center justify-center h-screen">
+            Sorry, this page is still under development.
+            <Button onClick={handleDashboard} className="mt-4">
+                Back to Dashboard
+            </Button>
+        </div>
     )
 
 }
