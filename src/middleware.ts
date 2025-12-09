23c31c51f1b55804c0870a8c6d2e058bbff41bbd
ahmadammarm <ahmad.ammar.2205356@students.um.ac.id>
2025-12-09 17:24:05 +0700

    fix: middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,11 @@
-// middleware.ts
-export { default } from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    pages: {
+        signIn: "/auth/sign-in",
+    },
+});
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/wizard'],
+    matcher: ['/dashboard/:path*'],
 };
